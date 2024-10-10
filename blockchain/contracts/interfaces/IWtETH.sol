0fc468530c41a2f06fa4e19596dffeac35b323bc
3NFBagdu <gbagh16@freeuni.edu.ge>
2024-10-10 18:43:05 +0200

    feat: add wtETH

---
+++ blockchain/contracts/interfaces/IWtETH.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.27;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IWtETH is IERC20 {
+
+    function wrap(uint256 _tETHAmount) external returns (uint256);
+    function unwrap(uint256 _wtETH) external returns (uint256);
+    function getTETHByWeETH(uint256 _wtETH) external view returns (uint256);
+
+}
