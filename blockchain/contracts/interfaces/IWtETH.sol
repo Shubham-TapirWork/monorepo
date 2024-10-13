baca603d7bd3c61ad670aa0741a44841b452aee2
3NFBagdu <gbagh16@freeuni.edu.ge>
2024-10-14 01:23:43 +0200

    feat: add wrap/unwrap testing

--- blockchain/contracts/interfaces/IWtETH.sol
+++ blockchain/contracts/interfaces/IWtETH.sol
@@ -7,6 +7,6 @@ interface IWtETH is IERC20 {
 
     function wrap(uint256 _tETHAmount) external returns (uint256);
     function unwrap(uint256 _wtETH) external returns (uint256);
-    function getTETHByWeETH(uint256 _wtETH) external view returns (uint256);
+    function getTETHByWtETH(uint256 _wtETH) external view returns (uint256);
 
 }
