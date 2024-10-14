28f2c43d0196dae9a67e2003f42a3607afefe261
alexursol2 <103754814+alexursol2@users.noreply.github.com>
2024-10-14 22:51:43 +0200

    Create ERC20.sol
    Create an erc20 token for a mock DEX contract

---
+++ blockchain/contracts/mock/ERC20.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Balloons is ERC20 {
+  construct() ERC20("Anytoken", "ANT") {
+    _mint(msg.sender, 1000 ether);
+  }
+}
